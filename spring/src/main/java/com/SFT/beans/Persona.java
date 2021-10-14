79670bae844fddfaaabc75bb595b0d2f0357c64d
Federico 'Magus' Rocca <frocca17@gmail.com>
2021-10-14 15:39:12 -0300

    Spring Framework Tutorial - 15
    Spring Framework Tutorial - 15 PostConstruct y PreDestroy

--- spring/src/main/java/com/SFT/beans/Persona.java
+++ spring/src/main/java/com/SFT/beans/Persona.java
@@ -1,5 +1,8 @@
 package com.SFT.beans;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 public class Persona {
 	
 	private int id;
@@ -8,11 +11,13 @@ public class Persona {
 	private Pais pais;
 	private Ciudad ciudad;
 	
+	@PostConstruct
 	private void initBean()
 	{
 		System.out.println("Antes de inicializar el bean");
 	}
 	
+	@PreDestroy
 	private void destroyBean()
 	{
 		System.out.println("Bean a punto de ser destruido");
