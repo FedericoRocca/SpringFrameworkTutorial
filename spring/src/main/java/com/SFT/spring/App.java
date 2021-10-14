93817011cbaba6398951757d7ae2333fca42cb10
Federico 'Magus' Rocca <frocca17@gmail.com>
2021-10-14 15:08:16 -0300

    Spring Framework Tutorial - 12
    Spring Framework Tutorial - 12 Bean Scopes

--- spring/src/main/java/com/SFT/spring/App.java
+++ spring/src/main/java/com/SFT/spring/App.java
@@ -16,6 +16,15 @@ public class App {
 		System.out.println(m.getSaludo());
 		
 		Persona per = (Persona) appContext.getBean("persona");
+		Persona per2 = (Persona) appContext.getBean("persona");
+		
+		per.setId(1);
+		per.setNombre("persona " + per.getId() + " nombre");
+		per.setApodo("apodo persona " + per.getId());
+		
+		per2.setId(2);
+		per2.setNombre("persona " + per2.getId() + " nombre");
+		per2.setApodo("apodo persona " + per2.getId());
 		
 		System.out.println(
 				"ID: " + per.getId() + 
@@ -24,6 +33,16 @@ public class App {
 				", pais: " + per.getPais().getNombre() + 
 				", ciudad: " + per.getCiudad().getNombre()
 				);
+		System.out.println(per);
+		
+		System.out.println(
+				"ID: " + per2.getId() + 
+				", nombre: " + per2.getNombre() + 
+				", apodo: " + per2.getApodo() + 
+				", pais: " + per2.getPais().getNombre() + 
+				", ciudad: " + per2.getCiudad().getNombre()
+				);
+		System.out.println(per2);
 		
 		((ConfigurableApplicationContext)appContext).close();
 	}
